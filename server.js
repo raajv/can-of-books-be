91f7f33bab357d21ea8aaf5b6d03000e54be475f
raajv <raaj.vardhan@gmail.com>
2021-11-11 01:31:33 +0530

    added get/post/del

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-// import our library that we are going to use to interact (make queries and stuff) with our mongo db
 const mongoose = require('mongoose');
 const Book = require ('./bookmodel.js');
 const app = express();
 app.use(cors());
+app.use(express.json())
 
 
 
@@ -18,33 +18,45 @@ db.once('open', function() {
 const PORT = process.env.PORT
 app.get('/', (req, res) => res.send('hello'));
 app.get('/books', handleBooks);
-app.post('/books',handlePostBooks)
+app.post('/books',handlePostBooks);
+app.delete('/books',handleDelBooks);
 
 async function handleBooks (req,res){
 
 try {
-  let booksFromDB = await Book.find({});
+  const email = req.query.email;
+  let booksFromDB = await Book.find({email});
   if (booksFromDB) {
-  // if the cat cat reqest/query was successful send the cats
   res.status(200).send(booksFromDB);
   } else {
     res.status(404).send('no books for you');
   }
-
 } catch (e) {
   console.error(e);
   res.status(500).send('server error')
 }
-}
+};
 
 async function handlePostBooks(req,res){
   try{
     let newBook = await Book.create(req.body)
     res.status(200).send(newBook);
-
-  }catch(e){
+}catch(e){
     res.status(500).send('no book added')
   }
+}
+
+async function handleDelBooks(req,res){
+const id = req.params.id;
+const email = req.params.email;
+
+const delBook= await Book.findOne({_id: id , email: email});
+if(!delBook){
+  res.status(400).send('no book to del')
+}else{
+  await Book.findByIdAndDelete(id);
+  res.status(200).send('bye book')
+}
 }
 
 
