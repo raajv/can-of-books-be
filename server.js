7fc8ca15323b6700b1cf2601635cf68950a1d272
raajv <raaj.vardhan@gmail.com>
2021-11-15 21:22:00 +0530

    added auth

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const verifyUser = require('./auth');
 const mongoose = require('mongoose');
 const Book = require ('./bookmodel.js');
 const app = express();
@@ -20,22 +21,29 @@ app.get('/', (req, res) => res.send('hello'));
 app.get('/books', handleBooks);
 app.post('/books',handlePostBooks);
 app.delete('/books/:id',handleDelBooks);
-app.put('/books/:id',handlePutBooks)
+app.put('/books/:id',handlePutBooks);
+app.get('/user', getUser);
 
-async function handleBooks (req,res){
-
-try {
-  const email = req.query.email;
-  let booksFromDB = await Book.find({email});
-  if (booksFromDB) {
-  res.status(200).send(booksFromDB);
-  } else {
-    res.status(404).send('no books for you');
-  }
-} catch (e) {
-  console.error(e);
-  res.status(500).send('server error')
-}
+function handleBooks (request,response){
+verifyUser(request, async(err, user) => {
+    if (err) {
+      response.send('invalid token');
+    } else {
+      
+      try {
+        const email = user.email;
+        let booksFromDB = await Book.find({email});
+        if (booksFromDB) {
+        response.status(200).send(booksFromDB);
+        } else {
+          response.status(404).send('no books for you');
+        }
+      } catch (e) {
+        console.error(e);
+        response.status(500).send('server error')
+      }
+    }
+  })
 };
 
 async function handlePostBooks(req,res){
@@ -55,21 +63,28 @@ async function handlePostBooks(req,res){
   }
 }
 
-async function handleDelBooks(req,res){
-  const id = req.params.id;
-  const email = req.query.email;
-try {
-    const delBook = await Book.findOne({ _id: id, email: email });
-    if (!delBook) {
-      res.status(400).send('could not delete book');
+function handleDelBooks(req,res){
+  verifyUser(request, async(err, user) => {
+    if (err) {
+      response.send('invalid token');
     } else {
-      await Book.findByIdAndDelete(id);
-      res.status(204).send('book gone ');
+      
+      const id = req.params.id;
+      const email = user.email;
+    try {
+        const delBook = await Book.findOne({ _id: id, email: email });
+        if (!delBook) {
+          res.status(400).send('could not delete book');
+        } else {
+          await Book.findByIdAndDelete(id);
+          res.status(204).send('book gone ');
+        }
+      } catch (e) {
+        console.log(e);
+        res.status(500).send('server error');
+      }
     }
-  } catch (e) {
-    console.log(e);
-    res.status(500).send('server error');
-  }
+  })
 }
 
 
@@ -90,6 +105,17 @@ async function handlePutBooks(req, res) {
     res.status(500).send('server error');
   }
 
+}
+
+function getUser(request, response) {
+
+  verifyUser(request, (err, user) => {
+    if (err) {
+      response.send('invalid token');
+    } else {
+      response.send(user);
+    }
+  })
 }
 
 app.listen(PORT,() =>console.log(`im listening on ${PORT}`) )
